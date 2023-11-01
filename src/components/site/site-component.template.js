b9646c485b0ab09c51b35e42f508d798321b6ca1
NikitaZ14 <pitoniyglaz@yandex.ru>
2023-11-02 01:43:21 +0700

    Сделал компоненты хидера и первой секции.

--- src/components/site/site-component.template.js
+++ src/components/site/site-component.template.js
@@ -5,7 +5,10 @@ export default `
     <main class="main">
         <header-component></header-component>
         <best-in-auto-component></best-in-auto-component>
+<<<<<<< HEAD
         <why-choose-us-component></why-choose-us-component>
         <rent-a-car-component></rent-a-car-component>
+=======
+>>>>>>> a014494 (Сделал компоненты хидера и первой секции.)
     </main>
 `;
